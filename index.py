2cc6d8b738a2b7095def92a5e29c7eda6cda5ea8
hkprasadam <hemanthkumarprasadam48@gmail.com>
2024-12-21 12:59:51 +0530

    add operators

--- index.py
+++ index.py
@@ -1,2 +1,24 @@
-list=[0,1,2,3]
-print(str(list))
+# list=[0,1,2,3]
+# print(str(list))
+   
+# --------operators---------
+# -----And---------
+
+print(-1 and -3)
+print(-3 and 0)
+print(-1 and False)
+print(False and 0)
+
+# --------OR--------
+
+print(-2 or 0)
+print(0 or False)
+print(True or 3)
+
+# ---------bitwise------
+
+# 1011
+# 1 * 1 + 1 * 2 + 0 * 4 + 1 * 6 = 1+2+0+6 = 9 ----binary
+# 1 * 1 + 1 * 8 + 0 * 64 + 1 * 512 = 1+8+0+512= 521-----octa
+
+print(0x1b)

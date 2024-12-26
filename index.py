e7bb8e4fbfaea43961fa723e874ff71c7719135d
hkprasadam <hemanthkumarprasadam48@gmail.com>
2024-12-26 12:52:42 +0530

    add nested loops

--- index.py
+++ index.py
@@ -1,24 +1,50 @@
-# list=[0,1,2,3]
-# print(str(list))
+# # list=[0,1,2,3]
+# # print(str(list))
    
-# --------operators---------
-# -----And---------
+# # --------operators---------
+# # -----And---------
 
-print(-1 and -3)
-print(-3 and 0)
-print(-1 and False)
-print(False and 0)
+# print(-1 and -3)
+# print(-3 and 0)
+# print(-1 and False)
+# print(False and 0)
 
-# --------OR--------
+# # --------OR--------
 
-print(-2 or 0)
-print(0 or False)
-print(True or 3)
+# print(-2 or 0)
+# print(0 or False)
+# print(True or 3)
 
-# ---------bitwise------
+# # ---------bitwise------
 
-# 1011
-# 1 * 1 + 1 * 2 + 0 * 4 + 1 * 6 = 1+2+0+6 = 9 ----binary
-# 1 * 1 + 1 * 8 + 0 * 64 + 1 * 512 = 1+8+0+512= 521-----octa
+# # 1011
+# # 1 * 1 + 1 * 2 + 0 * 4 + 1 * 6 = 1+2+0+6 = 9 ----binary
+# # 1 * 1 + 1 * 8 + 0 * 64 + 1 * 512 = 1+8+0+512= 521-----octa
 
-print(0x1b)
+# print(0x1b)
+
+# --------nested loops---------
+
+# i=360
+
+# if i < 350:
+#      print(i*10)
+# else:
+#      print(i*20)
+
+i=60
+
+if i<350 :
+    if i<=200 :
+            if i<=100:
+                print(i*-5)
+            else:
+                print(0)    
+    else :
+        print(i*10) 
+
+
+else :
+    print (i*20)   
+
+
